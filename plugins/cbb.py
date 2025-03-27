cff4fecc4522b17eecf573173aaad4515d670563
NovaxKing1 <kiirunicky@gmail.com>
2025-03-27 12:56:20 +0300

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -12,25 +12,25 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     if data == "about":
         await query.message.edit_text(
             text=(
-                "🎬 <b>Welcome to iBOX TV!</b>\n\n"
-                "🌟 **Your Ultimate Movie & TV Show Destination**\n"
+                "🎬 <b>Welcome to K-ZEN TV!</b>\n\n"
+                "🌟 **Home for All your Favorite & Latest Movies & TV Shows**\n"
                 "🔹 Stay updated with the **latest movies & series**\n"
                 "🔹 Search and discover content **effortlessly**\n"
                 "🔹 Get **exclusive updates & recommendations**\n\n"
                 "💡 **Our Community & Updates:**\n"
-                "📢 <b>Owner:</b> <a href='tg://user?id={OWNER_ID}'>iBOX TV</a>\n"
-                "📺 <b>Updates:</b> <a href='https://t.me/iBOX_TV'>iBOX TV</a>\n"
-                "🎥 <b>Movies Channel:</b> <a href='https://t.me/iBOXTVMOVIES'>iBOX TV FAMILY</a>\n"
-                "🌍 <b>Community:</b> <a href='https://t.me/+Cze71ohH6B82ZTZk'>Search Movies</a>\n"
-                "🔎 <b>Movie Search Chat:</b> <a href='https://t.me/+ESw_v3HM6nRlNTQ0'>iBOX TV</a>\n\n"
+                "📢 <b>Owner:</b> <a href='tg://user?id={OWNER_ID}'>K-ZEN TV</a>\n"
+                "📺 <b>Updates:</b> <a href='https://t.me/K_ZenTv'>K-ZEN TV</a>\n"
+                "🎥 <b>Movies Channel:</b> <a href='https://t.me/K_ZenMovies'>K-ZEN MOVIES</a>\n"
+                "🌍 <b>Community:</b> <a href='https://t.me/+npjbj_z59uYyZTM0'>Request Series</a>\n"
+                "🔎 <b>Movie Search Chat:</b> <a href='https://t.me/+bc4ZizsFnoZjYzU0'>K-Zen Movie Requests</a>\n\n"
                 "✨ **Enjoy a seamless movie experience with us!** 🍿🎥"
             ).format(OWNER_ID=OWNER_ID),
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🎬 Explore Movies", url="https://t.me/iBOXTVMOVIES"),
-                        InlineKeyboardButton("📢 Latest Updates", url="https://t.me/iBOX_TV")
+                        InlineKeyboardButton("🎬 Explore Movies", url="https://t.me/K_ZenMovies"),
+                        InlineKeyboardButton("📢 Latest Updates", url="https://t.me/K_ZenTv")
                     ],
                     [
                         InlineKeyboardButton("❌ Close", callback_data="close")
